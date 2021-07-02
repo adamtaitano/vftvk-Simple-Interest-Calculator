916f67ce497fd8ad557b3fe92eb133db895ce0a9
adam-taitano <79883367+adam-taitano@users.noreply.github.com>
2021-07-02 10:57:27 -0700

    Add script functions
    Validate() - alert if amount is not positive,
    compute() - calculate total with interest, and
    updateRate() - updates rate % value according to slider input.

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+
+function compute() {
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = principal * (1 + years * (rate / 100));
+  var year = new Date().getFullYear() + parseInt(years);
+  var result = "\n If you deposit "+principal+",\n at an interest rate of "+rate+"%. \n You will receive an amount of "+interest+", \n in the year "+year+"\n\n";
+  document.getElementById("result").innerText=result;
 }
-        
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function validate(number) {
+  if (number <= 0)
+    alert("Enter a positive number.");
+}
